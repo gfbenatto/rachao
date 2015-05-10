c9b6dda126c1fea50bc027caa9c29b9d80fac297
torlax <torlax@gmail.com>
2015-05-09 21:50:19 -0300

    Updating rachao.py

---
+++ rachao.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+pessoas = input('Digite o numero de pessoas  ')
+
+# quantidade de carne por pessoa
+carne = 0.3
+
+# quantidade de cerveja por pessoa
+cerveja = 1.5
+
+resultadocarne = pessoas*carne
+
+print('A quantidade de carne necessaria resultadocarne e', resultadocarne, 'Kilos de carne')
+
+resultadocerveja = pessoas*cerveja
+
+print('A quantidade de cerveja necessaria e', resultadocerveja, 'litros de cerveja')
+
+
+
