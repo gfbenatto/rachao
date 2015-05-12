be4eb17289c3d26a4618646f6940b72a67e4e9b4
torlax <torlax@gmail.com>
2015-05-12 20:30:24 -0300

    Codigo rachao.py atualizado para receber strings

--- rachao.py
+++ rachao.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-pessoas = input('Digite o numero de pessoas  ')
 
 # quantidade de carne por pessoa
 carne = 0.3
@@ -8,13 +7,12 @@ carne = 0.3
 # quantidade de cerveja por pessoa
 cerveja = 1.5
 
-resultadocarne = pessoas*carne
-
-print('A quantidade de carne necessaria resultadocarne e', resultadocarne, 'Kilos de carne')
-
-resultadocerveja = pessoas*cerveja
-
-print('A quantidade de cerveja necessaria e', resultadocerveja, 'litros de cerveja')
-
-
+pessoas = raw_input("Digite o numero de pessoas: ")
+if pessoas.isdigit() == False:
+    print("Ops voce digitou algo que nao entendo, so sei fazer conta com numerais!!!!")
+else:
+    resultadocarne = float(pessoas)*carne
+    print('A quantidade de carne necessaria resultadocarne e', resultadocarne, 'Kilos de carne')
+    resultadocerveja = float(pessoas)*cerveja
+    print('A quantidade de cerveja necessaria e', resultadocerveja, 'litros de cerveja')
 
